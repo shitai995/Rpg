d862f4cc1a3f0f2a53b77219ae06ca3d9de71260
shitai995 <16622140921@163.com>
2025-12-10 00:56:35 +0800

    添加背景动态效果以及地图初步创建

--- Assets/Script/ParallaxBackground.cs
+++ Assets/Script/ParallaxBackground.cs
@@ -5,19 +5,47 @@
 // 描述：
 // ========================================================
 
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Camera mainCamera;
+    private float lastCameraPositionX;
+    private float cameraHalfWidth;
+
+    [SerializeField] private ParallaxLayer[] backgroundlayers;
+
+    private void Awake()
     {
-        
+        mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        InitializeLayers();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float currentCameraPositionX = mainCamera.transform.position.x;
+        float distanceToMove = currentCameraPositionX - lastCameraPositionX;
+        lastCameraPositionX = currentCameraPositionX;
+
+
+        float cameraLeftEdge = currentCameraPositionX - cameraHalfWidth; ;
+        float cameraRightEdge = currentCameraPositionX + cameraHalfWidth;   
+
+        foreach(ParallaxLayer layer in backgroundlayers)
+        {
+            layer.Move(distanceToMove);// 背景跟随相机
+            layer.LoopBackground(cameraLeftEdge, cameraRightEdge);// 循环背景
+        }
     }
+    /// <summary>
+    /// 计算图像宽度
+    /// </summary>
+    private void InitializeLayers()
+    {
+        foreach (ParallaxLayer layer in backgroundlayers)
+            layer.CalculateImageWidth();
+    }
+        
 }
--- Assets/Script/ParallaxLayer.cs
+++ Assets/Script/ParallaxLayer.cs
@@ -1,16 +1,44 @@
+// ========================================================
+// 作者：娇娇 
+// 创建时间：2025-12-09 01:15:59
+// 版本：V1.1
+// 描述：
+// ========================================================
+
 using UnityEngine;
 
-public class ParallaxLayer : MonoBehaviour
+
+[System.Serializable]
+public class ParallaxLayer
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField] private Transform background;
+    [SerializeField] private float parallaxMultiplier;//视觉差数
+    [SerializeField] private float imageWidthOffset = 10;// 背景偏移补偿
+ 
+    private float imageFullWidth;
+    private float imageHalfWidth;
+
+
+    public void CalculateImageWidth()
     {
+        imageFullWidth = background.GetComponent<SpriteRenderer>().bounds.size.x;
+        imageHalfWidth = imageFullWidth / 2;
+    }
+    public void Move(float distanceToMove)
+    {
+        background.position += Vector3.right * (distanceToMove * parallaxMultiplier);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoopBackground(float cameraLeftEdge,float cameraRightEdge)
     {
-        
+        float imageRightEdge = (background.position.x + imageHalfWidth) - imageWidthOffset;
+        float imageLeftEdge = (background.position.x - imageHalfWidth) + imageWidthOffset;
+
+        if (imageRightEdge < cameraLeftEdge)
+            background.position += Vector3.right * imageFullWidth;
+        else if (imageLeftEdge > cameraRightEdge)
+            background.position += Vector3.left * -imageFullWidth;
     }
 }
